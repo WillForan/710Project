956b12583f86f98e4440525b52fee61d7c8b839d
Will Foran <wforan1@andrew.cmu.edu>
2011-04-28 12:47:21 -0400

    better format of plasmo annotations

--- scripts/_getAnnotations.sh
+++ scripts/_getAnnotations.sh
@@ -3,8 +3,8 @@
 #PLASMO
 curl curl http://plasmodb.org/common/downloads/release-7.1/Pfalciparum/txt/PfalciparumGene_PlasmoDB-7.1.txt | 
 grep "Genomic Location:"|
-sed s/,//g | 
-sort -t' ' -k3.7n > ../data/CDS/plasmoSorted.txt
+sed s/,//g | awk '(/Pf3D7_/){print substr($3,7,2), $4, $6, substr($7,2,1)}' plasmoSorted.txt |sed s/^0// |
+sort -n -t' ' > ../data/CDS/plasmoSorted.txt 
 
 #C El
 wget ftp://ftp.wormbase.org/pub/wormbase/genomes/c_elegans/genome_feature_tables/GFF3/current.gff3.gz
