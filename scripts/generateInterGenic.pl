dc6206ec192794c82f2a6836cf68bb90737f99ad
Will Foran <wforan1@andrew.cmu.edu>
2011-04-28 10:56:25 -0400

    genIterGen does both strands

--- scripts/generateInterGenic.pl
+++ scripts/generateInterGenic.pl
@@ -5,15 +5,14 @@ use strict; use warnings;
 #eg ./generateInterGenic.pl > ../bridge/plasmo_intergenic.fasta
 #
 #
-open my $genefile, '/afs/andrew.cmu.edu/usr/wforan1/project/data/CDS/plasmoSorted.txt' or die "Cannot read gene file: $!\n";
-#open my $genefile, 'tmptestannot' or die "Cannot read gene file: $!\n";
-
+my $annotationfile='/afs/andrew.cmu.edu/usr/wforan1/project/data/CDS/plasmoSorted.txt';
+my $chromprefix='/afs/andrew.cmu.edu/usr/wforan1/project/data/genome/chr';
 #id label 
 my $id=0;
 #start with + reads
-my $sense='\(\+\)';
+#my $sense='\(\+\)';
 #start on chr1
-my $gene_chr=1;
+my $gene_chr;
 
 #need genome and genpos to be globalish
 my $genome;
@@ -24,7 +23,7 @@ my @nts;
 sub openchr{
     close($genome) if($genome);
     my $chr=shift;
-    open $genome, "/afs/andrew.cmu.edu/usr/wforan1/project/data/genome/chr$chr.fa" or die "Cannot read chr$chr.fa: $!\n";
+    open $genome, "$chromprefix$chr.fa" or die "Cannot read chr$chr: $!\n";
     #open $genome, "tmptestseq" or die "Cannot read chr$chr.fa: $!\n";
     scalar(<$genome>); #read first line (not part of seq)
     $genpos=1;
@@ -32,52 +31,65 @@ sub openchr{
 }
 
 
+for my $strand ('\(\+\)','\(\-\)'){
 
-openchr $gene_chr;
+    open my $annoteFH, $annotationfile  or die "Cannot read gene file: $!\n";
+    $gene_chr=1;
+    openchr $gene_chr;
+    my $intergen="";
 
-#for each line
-while(<$genefile>){
- #line must be the right sense and have an assocated chrome
- next if(!m/$sense$/ || !m/Pf3D7_0*(\d+)/);
- my $chrom=$1;
- 
- #make sure were reading the right file;
- if($chrom!=$gene_chr){
-    #print the rest of the genome as one id
-    $id++;
-    print "\n>$id;chr$gene_chr:$genpos-rest\n";
-    print <$genome>; 
+    #for each line
+    while(<$annoteFH>){ 
+     #line must be the right sense and have an assocated chrome
+     next if(!m/$strand$/ || !m/Pf3D7_0*(\d+)/);
+     my $chrom=$1;
+     
+     #make sure were reading the right file;
+     if($chrom!=$gene_chr){
+	#print the rest of the genome as one id
+	$id++;
+	print "\n>$id;chr$gene_chr:$genpos-rest\n";
+	print <$genome>; 
 
-    #open the next chrm
-    openchr $chrom 
- }
+	#open the next chrm
+	openchr $chrom 
+     }
 
- my ($start, $end) = (split /[ :-]/)[5,8];
- #print $chrom,":", $start,"-",$end,"\n";
+     my ($start, $end) = (split /[ :-]/)[5,8];
+     #print $chrom,":", $start,"-",$end,"\n";
 
- #start new fa ID
- $id++;
- print "\n>$id;chr$chrom:$genpos-$start\n";
+     #start new fa ID
+     $id++;
+     if($strand eq '\(\-\)'){
+	 print ">$id;chr$chrom:$start-$genpos\n";
+     }
+     else{ 
+	 print ">$id;chr$chrom:$genpos-$start\n";
+     }
 
-# #print all the nts up to the genomic region
- while($genpos<=$end ) {
-   #read in nt if none are cached (possible inf. loop here if input not formated well)
-   @nts=split //, <$genome> if($#nts < 0 && !eof($genome));
+    # #print all the nts up to the genomic region
+     while($genpos<=$end ) {
+       #read in nt if none are cached (possible inf. loop here if input not formated well)
+       @nts=split //, <$genome> if($#nts < 0 && !eof($genome));
 
-   my $nt = shift @nts;
+       my $nt = shift @nts;
 
-   #print it if its before start
-   print $nt if($genpos<$start);
+       #print it if its before start
+       #print $nt if($genpos<$start);
+       $intergen.=$nt;
 
-   #print but don't count newlines (or any other chacter not correct
-   $genpos++ if $nt =~ /[ATGC]/;
-   
- }
-# $genpos=$end+1;
-
-
-}
-print "\n";
+       #print but don't count newlines (or any other chacter not correct
+       $genpos++ if $nt =~ /[ATGC]/;
+       
+     }#end of intergen region
+     if($strand eq '\(\-\)'){
+       $intergen =~ tr/ATGC/TACG/;
+       $intergen = reverse $intergen;
+     }
+     print $intergen,"\n";
+     $intergen="";
+   }#end gene annotation file
+   close $annoteFH;
+}#end for each strand
 close $genome;
-close $genefile;
 
--- scripts/srna_display_results.sh
+++ scripts/srna_display_results.sh
@@ -9,5 +9,6 @@ perl -ne 'chomp;
 	if(m/Name: (.*)/){print $1," ";}
 	elsif(m/Position: (.*) \((\d)/){print "$1 $2 "}
 	elsif(m/Length: (\d+)/){print $1," "}
+	elsif(m/Strand: (\d+)/){print $1," "}
 	elsif(m/Score: (\d+)/){print $1,"\n"}
 	' $1 |sort -k 1,1n -k 2,2n -k 5,5n 
